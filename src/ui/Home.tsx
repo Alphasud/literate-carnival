63df3ca1557c8953fd13d19786456728a9c913b5
alphasud <c.denneulin@gmail.com>
2024-04-02 16:03:22 +0200

    style: add tailwind 🌈

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,11 +1,11 @@
 export default function Home(): JSX.Element {
   return (
     <div>
-      <h1>
+      <h1 className="font-semi-bold text-center text-xl text-yellow-500">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
       </h1>
     </div>
-  )
+  );
 }
